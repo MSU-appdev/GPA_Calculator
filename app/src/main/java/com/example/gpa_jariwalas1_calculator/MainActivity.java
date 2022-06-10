d17d50a8613bd0217c22e5aa2bc0aae6fd662f62
sarkoo71 <Sark173314@>
2022-06-09 21:19:37 -0400

    done without the clear activity

--- app/src/main/java/com/example/gpa_jariwalas1_calculator/MainActivity.java
+++ app/src/main/java/com/example/gpa_jariwalas1_calculator/MainActivity.java
@@ -2,13 +2,99 @@ package com.example.gpa_jariwalas1_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText ed1, ed2, ed3, ed4, ed5, ed6;
+    Button  btn1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ed1 = findViewById(R.id.gcourse1);
+        ed2 = findViewById(R.id.gcourse2);
+        ed3 = findViewById(R.id.gcourse3);
+        ed4 = findViewById(R.id.gcourse4);
+        ed5 = findViewById(R.id.gcourse5);
+        ed6 = findViewById(R.id.gpacount);
+
+        {
+            if (ed1.length() == 0) {
+                ed1.requestFocus();
+                ed1.setError("Field cannot be empty!");
+            }
+        }
+        if (ed2.length() == 0) {
+            ed2.requestFocus();
+            ed2.setError("Field cannot be empty!");
+        }
+        if (ed3.length() == 0) {
+            ed3.requestFocus();
+            ed3.setError("Field cannot be empty!");
+        }
+        if (ed4.length() == 0) {
+            ed4.requestFocus();
+            ed4.setError("Field cannot be empty!");
+
+        }
+        if (ed5.length() == 0) {
+            ed5.requestFocus();
+            ed5.setError("Field cannot be empty!");
+        }
+
+        btn1 =findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                CNT();
+
+            }
+        });
+
+    }
+
+    public  void CNT()
+    {
+        int m1, m2, m3, m4, m5;
+        int tot;
+        int avg;
+
+        m1 = Integer.parseInt(ed1.getText().toString());
+        m2 = Integer.parseInt(ed2.getText().toString());
+        m3 = Integer.parseInt(ed3.getText().toString());
+        m4 = Integer.parseInt(ed4.getText().toString());
+        m5 = Integer.parseInt(ed5.getText().toString());
+
+
+        tot = m1 + m2 + m3 + m4 + m5;
+        avg =tot/5;
+
+        if (avg < 60)
+        {
+            ed6.setBackgroundColor(Color.RED);
+            ed6.setText(String.valueOf(avg));
+        }
+        else if (avg > 60 && avg < 80)
+        {
+            ed6.setBackgroundColor(Color.YELLOW);
+            ed6.setText(String.valueOf(avg));
+        }
+        else if (avg >= 80 && avg <= 100)
+        {
+            ed6.setBackgroundColor(Color.GREEN);
+            ed6.setText(String.valueOf(avg));
+        }
+        else
+        {
+            ed6.setBackgroundColor(Color.BLUE);
+        }
     }
 }
